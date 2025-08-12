07975e85055549c23302123f9cfb103b78b54d37
mctipper <morgan.tipper@hotmail.com>
2025-08-12 06:36:25 +0000

    test: debugging logic

---
+++ src/debug/dice/getDiceBinaryActivationStats.debug.ts
@@ -0,0 +1,54 @@
+import { getDiceBinaryActivationStats } from "@logic/diceActivations/getDiceBinaryActivationStats";
+import type { Food } from "@customTypes";
+import { baseGameDie } from "@data/diceDefinitions";
+
+
+export function debug(): any {
+    type DebugCase = {
+        food: Food | Food[];
+        rolls: number;
+    };
+    type DebugOutputCase = DebugCase & {
+        result: {
+            distribution: Record<number, number>;
+            failure: number;
+            anySuccess: number;
+            expectedValue: number;
+        }
+    };
+
+    const outputs: DebugOutputCase[] = []
+
+    const cases: DebugCase[] = [
+        // one food on one face
+        { food: 'Fish', rolls: 1 },
+        { food: 'Fish', rolls: 5 },
+        { food: 'Rodent', rolls: 1 },
+        { food: 'Rodent', rolls: 5 },
+        { food: 'Fruit', rolls: 1 },
+        { food: 'Fruit', rolls: 5 },
+        // one food on two faces
+        { food: 'Seed', rolls: 1 },
+        { food: 'Seed', rolls: 3 },
+        // two food, each on one face
+        { food: ['Rodent', 'Fruit'], rolls: 1 },
+        { food: ['Rodent', 'Fruit'], rolls: 4 },
+        // two food, both on two faces (but one is shared)
+        { food: ['Invertebrate', 'Seed'], rolls: 1 },
+        { food: ['Invertebrate', 'Seed'], rolls: 2 },
+        // four food, mixture
+        { food: ['Rodent', 'Fruit', 'Fish', 'Seed'], rolls: 1 },
+        { food: ['Rodent', 'Fruit', 'Fish', 'Seed'], rolls: 5 },
+    ];
+
+    for (const _case of cases) {
+        const result = getDiceBinaryActivationStats(baseGameDie, _case.food, _case.rolls)
+        let output: DebugOutputCase = {
+            food: _case.food,
+            rolls: _case.rolls,
+            result: result
+        }
+        outputs.push(output)
+    }
+    return outputs
+}
