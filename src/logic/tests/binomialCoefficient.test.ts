f877f7a82f412f1c844e88fc53656aa5783f86d0
mctipper <morgan.tipper@hotmail.com>
2025-08-12 05:41:43 +0000

    tests: coeff unit tests

---
+++ src/logic/tests/binomialCoefficient.test.ts
@@ -0,0 +1,24 @@
+import { describe, it, expect } from 'vitest';
+import { binomialCoefficient } from '@logic/binomialCoefficient';
+
+describe('binomialCoefficient', () => {
+    it.each([
+        // base cases
+        [1, -1, 0],
+        [1, 2, 0],
+        [1, 0, 1],
+        [1, 1, 1],
+        [1000, 1000, 1],
+        // non-base
+        [5, 2, 10],
+        [6, 3, 20],
+        [7, 4, 35],
+        [10, 5, 252],
+        [12, 6, 924],
+        [20, 10, 184756],
+        [30, 15, 155117520],
+    ])('computes binomialCoefficient(%i, %i) = %i', (n, k, expected) => {
+        expect(binomialCoefficient(n, k)).toBeCloseTo(expected);
+    });
+
+})
