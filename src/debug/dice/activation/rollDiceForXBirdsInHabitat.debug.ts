b4f7cf8bcd84ae056102125ef154772e726de485
mctipper <morgan.tipper@hotmail.com>
2025-08-14 02:39:02 +0000

    design: naming of debug files

---
+++ src/debug/dice/activation/rollDiceForXBirdsInHabitat.debug.ts
@@ -0,0 +1,13 @@
+import { birdsRollDiceForXBirdsInHabitiat, getRollDiceForXBirdsInHabitiatActivation } from "@logic/diceActivations/rollDiceForXBirdsInHabitat";
+
+
+
+export function debug() {
+    debugger;
+
+    const results = getRollDiceForXBirdsInHabitiatActivation(birdsRollDiceForXBirdsInHabitiat);
+
+    debugger;
+
+    return results
+}
