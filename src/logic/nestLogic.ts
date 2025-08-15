055c0fa68fc033c20cbe77e0d664eadffdc8b574
mctipper <morgan.tipper@hotmail.com>
2025-08-15 06:57:56 +0000

    refactor: function naming

--- src/logic/nestLogic.ts
+++ src/logic/nestLogic.ts
@@ -1,6 +1,6 @@
 import type { Nest } from "@customTypes";
 
-export function nestMatches(birdNest: Nest, requiredNest: Nest): boolean {
+export function nestMatchLogic(birdNest: Nest, requiredNest: Nest): boolean {
     if (requiredNest === 'None') return birdNest === 'None';
     if (birdNest === 'Star') return true; // Star matches anything
     return birdNest === requiredNest;
--- src/logic/tests/nestLogic.test.ts
+++ src/logic/tests/nestLogic.test.ts
@@ -1,42 +1,42 @@
 import { describe, it, expect } from 'vitest';
-import { nestMatches } from '@logic/nestLogic';
+import { nestMatchLogic } from '@logic/nestLogic';
 import type { Nest } from '@customTypes';
 
 const allNests: Nest[] = ['None', 'Cavity', 'Platform', 'Ground', 'Bowl', 'Star']
 const baseNests: Nest[] = ['Cavity', 'Platform', 'Ground', 'Bowl']
 
-describe('nestMatches', () => {
+describe('nestMatchLogic', () => {
     it('return based on matching with "None"', () => {
         for (const vn of allNests) {
             if (vn === 'None') {
                 // None is the only that will match with None
-                expect(nestMatches(vn, 'None')).toBe(true);
+                expect(nestMatchLogic(vn, 'None')).toBe(true);
             } else {
                 // all others fail match (including Star)
-                expect(nestMatches(vn, 'None')).toBe(false)
+                expect(nestMatchLogic(vn, 'None')).toBe(false)
             }
         }
     });
 
     it('return true based on baseNest matching self', () => {
         for (const bn of baseNests) {
-            expect(nestMatches(bn, bn)).toBe(true);
+            expect(nestMatchLogic(bn, bn)).toBe(true);
         }
     });
 
     it('return true based on Star matching baseNest', () => {
         for (const bn of baseNests) {
-            expect(nestMatches('Star', bn)).toBe(true);
+            expect(nestMatchLogic('Star', bn)).toBe(true);
         }
     });
 
     it('return true for Star matching Star', () => {
-        expect(nestMatches('Star', 'Star')).toBe(true);
+        expect(nestMatchLogic('Star', 'Star')).toBe(true);
     });
 
     it('return false for baseNest matching Star', () => {
         for (const bn of baseNests) {
-            expect(nestMatches(bn, 'Star')).toBe(false);
+            expect(nestMatchLogic(bn, 'Star')).toBe(false);
         }
     });
 }
